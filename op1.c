da0e70fe17e56969d34f4e46d0c4324c689836d8
muthu_the_hacker <a.muthuchidambaram2001@gmail.com>
2022-01-29 19:44:08 +0530

    day 2

---
+++ op1.c
@@ -0,0 +1,12 @@
+ #include<stdio.h>
+ int main()
+ {
+     int a,b;
+     a=5;
+     b=5;
+     printf("Addition of Two Number: %d \n",a+b);
+     printf("Substraction of Two Numbers: %d \n",a-b);
+     printf("Multiplication of Two Numbers: %d \n",a*b);
+     printf("Division of Two Numbers: %d \n",a/b);
+     printf("Modulo of Two Numbers: %d \n",a%b);
+ }
---
+++ op4.c
@@ -0,0 +1,11 @@
+// Logical Operators
+
+#include<stdio.h>
+int main()
+{
+    int a=2,b=3;
+    printf("a&b:%d \n",a&&b);
+    printf("a||b:%d \n",a||b);
+    printf("!(a>b):%d \n",!(a>b));
+}
+
