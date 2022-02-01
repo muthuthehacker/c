7daf153d5d94e2b390a430decf5f802078ec1ad6
muthu_the_hacker <a.muthuchidambaram2001@gmail.com>
2022-02-01 11:38:19 +0530

    new

---
+++ ctrl2.c
@@ -0,0 +1,15 @@
+//control structure if-else
+
+#include<stdio.h>
+int main()
+{
+    int a=3,b=5;
+    if(a==b)
+    {
+            printf("The two currency are eqaul :%d %d\n",a,b);
+    }
+        else{
+            printf("the currency is not same : %d %d \n",a,b);
+        }
+    
+}
